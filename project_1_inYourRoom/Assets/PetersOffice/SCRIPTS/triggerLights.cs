39f4c077e23d7edffd07b541b662fda391ca1e23
P10trH <peterhanulajr@gmail.com>
2017-09-26 15:01:39 -0500

    delte large files and repush

---
+++ project_1_inYourRoom/Assets/PetersOffice/SCRIPTS/triggerLights.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerLights : MonoBehaviour {
+
+	public GameObject[] lightSources;
+	public Renderer rend;
+	public bool toggleSwitch = false;
+
+	// Use this for initialization
+	void Start () {
+
+		lightSources = GameObject.FindGameObjectsWithTag("roomLight");
+
+		rend = GetComponent<Renderer>();
+
+		rend.material.SetColor("_Color", Color.red);
+		rend.material.SetColor("_EmissionColor", Color.red);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		print("hit lights");
+		
+		if (toggleSwitch == true) {
+
+			foreach (GameObject lamp in lightSources)
+        	{
+            	lamp.GetComponent<Light>().enabled=true;
+        	}
+
+			rend.material.SetColor("_Color", Color.red);
+			rend.material.SetColor("_EmissionColor", Color.red);
+
+			toggleSwitch = false;
+		}
+		else if (toggleSwitch == false) {
+
+			foreach (GameObject lamp in lightSources)
+        	{
+            	lamp.GetComponent<Light>().enabled=false;
+        	}
+
+			rend.material.SetColor("_Color", Color.green);
+			rend.material.SetColor("_EmissionColor", Color.green);
+
+			toggleSwitch = true;
+		}
+  		
+	}
+}
---
+++ project_1_inYourRoom/Assets/PetersOffice/SCRIPTS/triggerSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerSound : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnTriggerEnter(Collider other){
+
+		print("hit");
+  		if (!audioSource.isPlaying){
+    		audioSource.Play();
+  		}
+	}
+}
